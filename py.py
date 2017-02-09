a69882fc4fab669c6686781c439a7844f64f7431
Dmitry Ponyatov <dponyatov@gmail.com>
2017-02-09 14:35:51 +0400

    - stack compiler
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- py.py
+++ py.py
@@ -39,10 +39,6 @@ def p_REPL_none(p):
 def p_REPL_recur(p):
     ' REPL : REPL ex '
     print p[2].dump()
-    print '.text',p[2].text
-    print '.data',p[2].data
-    print '.bss',p[2].bss
-    print '.stack',p[2].stack
 
 def p_ex_scalar(p):
     ' ex : scalar '
@@ -50,28 +46,19 @@ def p_ex_scalar(p):
 def p_scalar_sym(p):
     ' scalar : SYM '
     p[0] = p[1]
-    p[1].text = '\n\tptr\t%s' % p[1].val
-    p[1].bss = '\n\t%s: dw ?' % p[1].val
 def p_scalar_num(p):
     ' scalar : NUM '
     p[0] = p[1]
-    p[1].text = '\n\tlit\t%s' % p[1].val
 
 def p_eq(p):
     ' ex : ex EQ ex '
     p[0] = p[2] ; p[0] += p[1] ; p[0] += p[3]
-    p[0].text = p[1].text + p[3].text + '\n\teq'
-    p[0].bss = p[1].bss + p[3].bss
 def p_add(p):
     ' ex : ex ADD ex '
     p[0] = p[2] ; p[0] += p[1] ; p[0] += p[3]
-    p[0].text = p[1].text + p[3].text + '\n\tadd'
-    p[0].bss = p[1].bss + p[3].bss
 def p_mul(p):
     ' ex : ex MUL ex '
     p[0] = p[2] ; p[0] += p[1] ; p[0] += p[3]
-    p[0].text = p[1].text + p[3].text + '\n\tmul'
-    p[0].bss = p[1].bss + p[3].bss
     
 def t_error(t): print 'lexer/error',t
 def p_error(p): print 'parse/error',p
--- sym.py
+++ sym.py
@@ -1,16 +1,17 @@
 class Sym:
     tag = 'sym'
-    def __init__(self, V):
-        self.val = V ; self.nest = [] ; self.attr = {}
-        self.text = '' ; self.data = '' ; self.bss = '' ; self.stack = '' 
+    def __init__(self, V): self.val = V ; self.nest = [] ; self.attr = {}
     def __iadd__(self, o): self.nest.append(o) ; return self
     def __repr__(self): return self.head()
     def head(self):
         return '<%s:%s> @%s' % (self.tag, self.val, id(self))
     def dump(self, depth=0):
         S = '\n' + '\t' * depth + self.head()
-#         for i in self.attr: S += ' %s=%s' % (i, self.attr[i])
+        for i in self.attr:
+            S += '\n' + '\t' * (depth + 1) + i + ' ='
+            S += self.attr[i].dump(depth + 2)
         for j in self.nest: S += j.dump(depth + 1)
         return S
 class Num(Sym): tag = 'num'
+class Str(Sym): tag = 'str'
 class Op(Sym):  tag = 'op'
